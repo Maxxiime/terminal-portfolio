6742234c6d929f96d8fecca3fcaf247314f2d8af
Maxxiime <maxiime.l@gmail.com>
2026-08-24 22:10:52 -0400

    Use Markdown content in portfolio commands

--- src/src/components/commands/Welcome.tsx
+++ src/src/components/commands/Welcome.tsx
@@ -7,6 +7,7 @@ import { profile } from "../../data/profile";
 import { useContext } from "react";
 import { languageContext } from "../../App";
 import { uiText } from "../../i18n";
+import { getMarkdownSections } from "../../data/markdown";
 
 const renderWelcomeHint = (hint: string) => {
   const parts = hint.split(/(help|question)/g);
@@ -22,16 +23,21 @@ const renderWelcomeHint = (hint: string) => {
 const Welcome: React.FC = () => {
   const { locale } = useContext(languageContext);
   const copy = uiText[locale];
+  const markdownSections = locale === "fr" ? getMarkdownSections(locale, "welcome") : [];
+  const markdownProfile = markdownSections.find(section => section.title === profile.name);
+  const welcomeHeadline = markdownSections[0]?.title || copy.welcomeHeadline;
+  const markdownTagline = markdownProfile?.paragraphs[0] || profile.title;
+  const markdownHint = markdownProfile?.paragraphs[1] || copy.welcomeHint;
 
   return (
     <HeroContainer data-testid="welcome">
       <HeroMeta className="info-section">
-        <div>{copy.welcomeHeadline}</div>
+        <div>{welcomeHeadline}</div>
         <div aria-hidden="true">&nbsp;</div>
         <div>{profile.name}</div>
-        <div>{profile.title}</div>
+        <div>{markdownTagline}</div>
         <div aria-hidden="true">&nbsp;</div>
-        <div>{renderWelcomeHint(copy.welcomeHint)}</div>
+        <div>{renderWelcomeHint(markdownHint)}</div>
         <div aria-hidden="true">&nbsp;</div>
       </HeroMeta>
     </HeroContainer>
